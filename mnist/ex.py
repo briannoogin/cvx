b9b8b41138891f4d5349bd2cb8010808a6817bdd
sid-devic <siddartha0860@gmail.com>
2018-07-30 21:34:04 -0500

    fix up ex

--- mnist/ex.py
+++ mnist/ex.py
@@ -19,7 +19,9 @@ y_train = x_train[:100]
 #print(x_train.shape)
 
 # test points
-test = [(1,1), (2,2), (0,1), (1, 2), (3.5,3), (-4,4), (-8,4), (-2,0), (-6,2)]
+test = [(1,1), (2,2), (0,1), (1, 2),(3.5,3), (-8,4), (-2,0), (-6,2)]
+test = [(x[0]+5,x[1]) for x in test]
+print(test)
 #test = [(1,1), (2,2)]
 
 # minimize piecewise linear function
@@ -63,8 +65,8 @@ for x in range(len(test)):
 # straight outta boyd
 for i in range(len(test)):
     for j in range(len(test)):
-        constraints.append(y_hat["y_hat_{0}".format(i)] + g_i["g_{0}".format(i)] * (test[j][0] - test[i][0]) <= y_hat["y_hat_{0}".format(j)])
-
+        constraints += [y_hat["y_hat_{0}".format(i)] + g_i["g_{0}".format(i)] * (test[j][0] - test[i][0]) <= y_hat["y_hat_{0}".format(j)]]
+        
 # set up obj to be sum of squared error
 for i in range(len(test)):
     obj += ((test[i][1] - y_hat["y_hat_{0}".format(i)]) ** 2)
@@ -78,14 +80,17 @@ prob.solve(verbose=True)
 sol = []
 # print optimal linear values
 for i in range(len(g_i)):
-    print("g_{0}".format(i),": ", g_i["g_{0}".format(i)].value)
+    #print(g_i["g_{0}".format(i)].value,',', end=" ")
     sol.append(float(g_i["g_{0}".format(i)].value))
-
+print(" ")
 # debug --
-x_0 = test[0][0]
-y_0 = test[0][1]
-
+x_0 = test[2][0]
+y_0 = test[2][1]
+maxf = 'max{'
 for g_i in range(len(sol)):
-    sol[g_i] = sol[g_i] * x_0
+    print('f{0}(x)='.format(g_i),test[g_i][1], '+', sol[g_i], '*(', 'x','-',test[g_i][0],')')
+    maxf += ('f{0}(x),'.format(g_i))
+    sol[g_i] = test[g_i][1] + sol[g_i] * (x_0 - test[g_i][0])
 
-print(x_0, " ", np.amax(sol))
+print(maxf)
+print(y_0, " ", np.amax(sol))
