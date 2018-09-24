53c12445bf1f38f5056cb7768235355b706c1d2c
sid-devic <siddartha0860@gmail.com>
2018-09-24 12:29:38 -0500

    s^t grad(f) correction

--- frank-wolfe/cvx_proj.py
+++ frank-wolfe/cvx_proj.py
@@ -1,44 +1,34 @@
 import numpy as np
 
-def n_distance(a, b):
-    d = 0
-    for x in range(len(a)):
-        d += a[x] - b[x]
-    
-    return d
-
 def gradient_direction(hull, test_point):
     # return the direction of the gradient. This method always returns a point
-    # from the hull, so instead we return the index of the point in hull to move towards.
-    gradient = [2*(n_distance(hull[x], test_point)) for x in range(len(hull))]
+    # from the hull, so instead we return the index of the point in hull to move towards. 
+    gradient = [(np.subtract(vertex, test_point)) for vertex in hull]
+    gradient = 2*sum(gradient)
 
-    # correct point to move towards is the minimum index of gradient
-    direction = gradient.index(min(gradient))
-    #print(gradient)
+    s_ks = [np.dot(hull[v], gradient) for v in range(len(hull))]
 
-    return direction
+    # correct point to move towards is the minimum of all s_ks, which is a vertex from our constraint set
+    return np.argmin(s_ks)
 
 def main():
     # our cvx hull
-    hull = np.array([[-4,2], [.5,-2.5], [6,3]])
-
-    # x coordinate of our test point. In this case, the corresponding y value should be 4
-    x = -6; y = 0;
+    hull = np.array([[2,1], [4,3], [1,1.5], [.5,5]])
+    
+    # test point
+    x = 1.2; y = 4;
     test_point = np.array([x,y])
 
-    # how close to get to correct answer
     epsilon = .01
     # if we want to arrive within epsilon of the correct answer, generally need 1/(epsilon^2) iter
-    iter = int(1 / (epsilon ** 2))
+    #iter = int(1 / (epsilon ** 2)) 
+    iter = 100
 
-    for z in range(1, iter):
+    for z in range(iter):
         step = 2 / (z + 2)
-        min_gradient_index = gradient_direction(hull,test_point)
-        print(np.subtract(hull[min_gradient_index], test_point)) 
-        test_point = np.add(test_point, step * (np.subtract(hull[min_gradient_index], test_point)))
-        #print(z,'/',iter)
+        s_k = gradient_direction(hull,test_point)
+        test_point = test_point +  step * (np.subtract(hull[s_k], test_point))
     
-    print('======')
     print(test_point)
 
 if __name__ == "__main__":
