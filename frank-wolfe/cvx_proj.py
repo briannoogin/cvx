cc3fae09d88b7c6737639f60362f5e331f541753
sid-devic <siddartha0860@gmail.com>
2018-09-25 15:45:08 -0500

    fixed projection, working on C implementation

--- frank-wolfe/cvx_proj.py
+++ frank-wolfe/cvx_proj.py
@@ -1,35 +1,36 @@
 import numpy as np
 
-def gradient_direction(hull, test_point):
-    # return the direction of the gradient. This method always returns a point
-    # from the hull, so instead we return the index of the point in hull to move towards. 
-    gradient = [(np.subtract(vertex, test_point)) for vertex in hull]
-    gradient = 2*sum(gradient)
-
-    s_ks = [np.dot(hull[v], gradient) for v in range(len(hull))]
-
-    # correct point to move towards is the minimum of all s_ks, which is a vertex from our constraint set
-    return np.argmin(s_ks)
-
 def main():
     # our cvx hull
     hull = np.array([[2,1], [4,3], [1,1.5], [.5,5]])
     
-    # test point
-    x = 1.2; y = 4;
-    test_point = np.array([x,y])
+    # point we wish to project onto the set
+    p_point = np.array([3,5])
+
+    # "randomly" initialized point inside our convex set, set to the first vertex in our hull
+    curr_proj = hull[0]
 
     epsilon = .01
     # if we want to arrive within epsilon of the correct answer, generally need 1/(epsilon^2) iter
     iter = int(1 / (epsilon ** 2)) 
-    #iter = 100
 
     for z in range(iter):
+        # step size required for convergence
         step = 2 / (z + 2)
-        s_k = gradient_direction(hull,test_point)
-        test_point = test_point +  step * (np.subtract(hull[s_k], test_point))
+
+        # gradient is 2*(z-y) where z is our current proj and y is the point we are attempting to proj
+        gradient = 2*np.subtract(curr_proj, p_point)
+
+        # calc s^T grad for each S vertex in our convex hull
+        s_ks = [np.dot(vertex, gradient) for vertex in hull]
+
+        # move towards the vertex which minimizes this
+        s_k = np.argmin(s_ks)
+
+        # update step
+        curr_proj = curr_proj +  step * (np.subtract(hull[s_k], curr_proj))
     
-    print(test_point)
+    print(curr_proj)
 
 if __name__ == "__main__":
     main()
